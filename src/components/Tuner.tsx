d01ff3cc13ff6722a1d5de91551cffbd9d87d50b
Alexandre Delaunay <adelaunay@teclib.com>
2026-03-25 21:20:16 +0100

    fix build

--- src/components/Tuner.tsx
+++ src/components/Tuner.tsx
@@ -4,7 +4,7 @@ import { Label } from '@/components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 
 export interface TunerProps {
-  currentPitch: PitchResult | null;
+  currentPitch?: PitchResult | null;
 }
 
 type Tuning = {
